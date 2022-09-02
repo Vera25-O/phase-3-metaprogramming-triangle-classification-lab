9984337c23b9439c2eedda77a03a4543c6709a4e
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-09-03 01:08:54 +0300

    modified triangle.rb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
+  attr_reader :m, :n, :p
+
+  def initialize(m, n, p)
+    @m = m
+    @n = n
+    @p = p
+  end
+
+  def kind
+    validate_triangle
+    if m == n && n == p
+      :equilateral
+    elsif m == n || n == p || m == p
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+    [m, n, p].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    m + n > p && m + p > n && n + p > m
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
   # write code here
 end
